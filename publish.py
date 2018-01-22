7bea5bf7e53d25291f230eaec022c0318e552db1
pabloveron <pabloveron@users.noreply.github.com>
2018-01-22 14:42:36 -0300

    Update publish.py

--- publish.py
+++ publish.py
@@ -3,6 +3,6 @@ import paho.mqtt.client as mqtt
 import sys
 
 mqttc = mqtt.Client(client_id="python_pub", clean_session=False)
-mqttc.connect("172.17.0.2", 1883)
+mqttc.connect("162.0.0.10", 1883)
 mqttc.publish("broker/messages", sys.argv[1], qos=1)
 mqttc.loop(2) #timeout = 2s
